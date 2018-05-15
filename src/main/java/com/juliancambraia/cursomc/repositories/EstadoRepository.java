50f361ee5f1b98ba3c894f603d81e3186a1e6139
JulianFernando <juliancambraia@gmail.com>
2018-05-15 11:43:05 -0300

    Estado e Cidade

---
+++ src/main/java/com/juliancambraia/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.juliancambraia.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.juliancambraia.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado , Integer> {
+
+}
